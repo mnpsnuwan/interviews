3e48b607a35f5538cc24bbd8518bd7487e9d8bd4
mnpsnuwan <nuwanp124@gmail.com>
2023-10-31 00:18:28 -0700

    IM:DEV:Leetcode problems -  XOR, Bit Manipulation.

---
+++ leetcode/bit-manipulation/DecodeXORedArray.java
@@ -0,0 +1,44 @@
+// 1720. Decode XORed Array
+//Easy
+
+// There is a hidden integer array arr that consists of n non-negative integers.
+//
+// It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+//
+// You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+//
+// Return the original array arr. It can be proved that the answer exists and is unique.
+//
+
+// Example 1:
+// Input: encoded = [1,2,3], first = 1
+// Output: [1,0,2,1]
+// Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+
+// Example 2:
+// Input: encoded = [6,2,7,3], first = 4
+// Output: [4,2,0,7,4]
+//
+// Constraints:
+// 2 <= n <= 104
+// encoded.length == n - 1
+// 0 <= encoded[i] <= 105
+// 0 <= first <= 105
+
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length + 1;
+        int[] arr = new int[n];
+
+        arr[0] = first;
+        for(int i = 0; i < n - 1; i++){
+            arr[i + 1] = arr[i] ^ encoded[i];
+        }
+        return arr;
+    }
+}
+
+// Readmap
+// bitwise xor
+// (6) 0110 ^ (4) 0100 = 0010 (2) <<< logic
+// arr[i+1] = arr[i] + encoded[i]
